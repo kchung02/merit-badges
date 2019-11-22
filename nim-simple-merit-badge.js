a6c5957ee87c3b5ffcb1a2b688b4d6e4d846e1bc
kchung02 <54716697+kchung02@users.noreply.github.com>
2019-11-22 11:46:59 -0500

    Bugged/Untested
    Functions not working

--- nim-simple-merit-badge.js
+++ nim-simple-merit-badge.js
@@ -1,14 +1,13 @@
-//Nim Simple by Kim Chung (unfinished)//
+//Nim Simple by Kim Chung//
 
-var games = 0
+var games = 0;
 var again = true;
-while (again == true){
+//while (again == true){
 	var count = 0;
-	var turn = 0;
 	var next = games%2;
 	//while (count < 21){
 		if (next == 0){
-			count = cpuTurn();
+			//count = cpuTurn();
 			alert("The count is now "+count);
 			//next = nextSwitch();
 		}
@@ -19,22 +18,15 @@ while (again == true){
 		}
 	//}
 	//again = declareWinner();
-}
+//}
 
 function cpuTurn(){
-	turn = Math.floor(Math.random()*3)+1;
-	while (turn > 1){
-		if (count+turn < 21){
-			alert("CPU counted "+turn);
-			count += turn;
-			return count;
-		}
-		else {
-			alert("CPU counted "+turn);
-			count += turn;
-			return turn;
-		}
-	}
+    if (count == 19 || count == 20) turn = 1;
+    else if (count == 18) turn = Math.floor(Math.random()*2)+1;
+    else turn = Math.floor(Math.random()*3)+1;
+    alert("CPU counts "+turn+".");
+    count += turn;
+    return count;
 }
 
 function nextSwitch(){
@@ -45,43 +37,36 @@ function nextSwitch(){
 	else {
 		next = 0;
 		return next;
+	}
 }
 
 function userTurn(){
-	turn = prompt("Enter 1, 2, or 3.");
-	while (turn == 1 || turn == 2 || turn == 3){
-		count += turn;
-		return count;
+	var goodTurn = false;
+	while (goodTurn == false){
+		turn = prompt("Pick a count 1-3.");
+		if (turn == 1 || turn == 2 || turn == 3){
+			count += Number(turn);
+			goodTurn = true;
+		}
+		else {
+			alert("Invalid input, try again.");
+			goodTurn = false;
 	}
-	alert("Invalid input, try again.");
+	return count;
 }
 
 function declareWinner(){
-	if (next == 0){
-		alert("You lose!");
-		games++
-		again = prompt("Play again? y = yes");
-		if (again == "y"){
-			again = true;
-			return again;
-		}
-		else {
-			again = false;
-			alert("Thanks for playing!");
-			return again;
-		}
+	if (next == 0) alert("You lose!");
+	else alert("You win!");
+	games++
+	again = prompt("Play again? y = yes");
+	if (again == "y"){
+		again = true;
+		return again;
 	}
 	else {
-		alert("You win!");
-		games++
-		again = prompt("Play again? y = yes");
-		if (again == "y"){
-			again = true;
-			return again;
-		}
-		else {
-			again = false;
-			alert("Thanks for playing!");
-			return again;
-		}
+		again = false;
+		alert("Thanks for playing!");
+		return again;
+	}
 }
