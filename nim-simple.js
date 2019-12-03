65ee86a77308c20b8fdebdcec0f4340132cef620
kchung02 <54716697+kchung02@users.noreply.github.com>
2019-12-03 10:31:17 -0500

    Update nim-simple.js

--- nim-simple.js
+++ nim-simple.js
@@ -16,7 +16,7 @@ while (again == true){
 	next = games%2;
 	while (count < 21){
 		if (next == 0){
-			turn = CPUTurn();
+			turn = cpuTurn();
 			alert("The computer picked "+turn+".");
 			count += turn;
 			alert("The count is now "+count+".");
@@ -36,9 +36,13 @@ while (again == true){
 }
 
 /* Functions */
-function CPUTurn(){
-	turn = Math.floor(Math.random()*3)+1;
-	return turn;
+function cpuTurn(){
+    if (count == 19 || count == 20) turn = 1;
+    else if (count == 18) turn = Math.floor(Math.random()*2)+1;
+    else turn = Math.floor(Math.random()*3)+1;
+    alert("CPU counts "+turn+".");
+    count += turn;
+    return count;
 }
 
 function nextSwitch(next){
@@ -55,20 +59,33 @@ function nextSwitch(next){
 function userTurn(){
 	var goodTurn = false;
 	while (goodTurn == false){
-		turn = prompt("Pick a number 1-3");
-		if (turn > 0 && turn < 4){
+		turn = prompt("Pick a count 1-3");
+		if (turn == 1 || turn == 2 || turn == 3){
+			count += Number(turn);
 			goodTurn = true;
-			return Number(turn);
 		}
-		else alert("Guess is out of range, try again.");
+		else {
+			alert("Invalid input, try again.");
+			goodTurn = false;
 	}
+	return count;
 }
 
 function declareWinner(){
-	if (next == 0){
-		alert("You lose!");
-	}
+	if (next == 0) alert("You lose!");
 	else alert("You win!");
+	games++
+	again = prompt("Play again? y = yes");
+	if (again == "y"){
+		again = true;
+		return again;
+	}
+	else {
+		again = false;
+		alert("Thanks for playing!");
+		return again;
+	}
+}
 }
 
 function playAgain(){
