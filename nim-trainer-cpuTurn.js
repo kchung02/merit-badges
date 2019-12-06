fc577ae2ec12730f0617259fea15d7900ffe454c
kchung02 <54716697+kchung02@users.noreply.github.com>
2019-12-06 10:21:52 -0500

    Update nim-trainer-cpuTurn.js

--- nim-trainer-cpuTurn.js
+++ nim-trainer-cpuTurn.js
@@ -1,8 +1,12 @@
+// Test function cpuTurn
+
+// Manually input a current count
+// Display current count returned by cpuTurn
 var count = parseInt(prompt("Pick a count."));
 alert("The count is now "+count);
-var turn = cpuTrainer(count)
-
-function cpuTrainer(count){
+var turn = cpuTurn(count)
+// Define function cpuTurn, taking count as parameter
+function cpuTurn(count){
     if (count%4 != 0) turn = 4 - count%4;
     else if (count == 20) turn = 1;
     else turn = Math.floor(Math.random()*3)+1;
@@ -11,3 +15,9 @@ function cpuTrainer(count){
     alert("The count is now "+count);
     return count;
 }
+// Calculate CPU Turn when count is NOT a multiple of 4.  Use Mod (%)
+// Otherwise, if count is 20, CPU should count 1
+// Otherwise, CPU should give a random turn 1-3.
+// Alert what CPU counts
+// Add turn to count
+// return count
