112ba94ec49a8fa68e04b0f63315abff636cb37c
kchung02 <54716697+kchung02@users.noreply.github.com>
2019-12-09 10:59:11 -0500

    Update nim-trainer.js

--- nim-trainer.js
+++ nim-trainer.js
@@ -6,34 +6,34 @@ var again = true;
 
 /* Main */
 while (again == true){
-  var count = 0;
+	var count = 0;
 	var next = games%2;
 	while (count < 21){
 		if (next == 0){
-			count = cpuTurn();
+			count = cpuTurn(count);
 			next = nextSwitch();
 		}
 		else {
-			count = userTurn();
-			next = nextSwitch();
+			count = userTurn(count);
+			next = nextSwitch(next);
 		}
 	}
-	again = declareWinner();
+	again = declareWinner(again);
 }
 alert("Thanks for playing!");
 
 /* Functions */
 function cpuTurn(count){
-    if (count%4 != 0) turn = 4 - count%4;
-    else if (count == 20) turn = 1;
-    else turn = Math.floor(Math.random()*3)+1;
-    alert("CPU counts "+turn);
-    count += turn;
-    alert("The count is now "+count);
-    return count;
+	if (count%4 != 0) turn = 4 - count%4;
+	else if (count == 20) turn = 1;
+	else turn = Math.floor(Math.random()*3)+1;
+	alert("CPU counts "+turn);
+	count += turn;
+	alert("The count is now "+count);
+	return count;
 }
 
-function nextSwitch(){
+function nextSwitch(next){
 	if (next == 0){
 		next = 1;
 		return next;
@@ -44,12 +44,12 @@ function nextSwitch(){
 	}
 }
 
-function userTurn(){
+function userTurn(count){
 	var goodTurn = false;
 	while (goodTurn == false){
 		turn = prompt("Pick a count 1-3.");
 		if (turn == 1 || turn == 2 || turn == 3){
-			count += Number(turn);
+			count += parseInt(turn);
 			goodTurn = true;
 		}
 		else {
@@ -61,7 +61,7 @@ function userTurn(){
 	return count;
 }
 
-function declareWinner(){
+function declareWinner(again){
 	if (next == 0) alert("You lose!");
 	else alert("You win!");
 	games++
