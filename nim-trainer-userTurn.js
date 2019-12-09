1dbe0c51221479f021f3ac3ee7a58c190150f141
kchung02 <54716697+kchung02@users.noreply.github.com>
2019-12-09 11:22:59 -0500

    Update nim-trainer-userTurn.js

--- nim-trainer-userTurn.js
+++ nim-trainer-userTurn.js
@@ -1,10 +1,16 @@
-count = userTurn(count);
+var count = 20;
+alert("The count is now "+count);
+var count = userTurn(count);
 
 function userTurn(count){
 	var goodTurn = false;
 	while (goodTurn == false){
 		turn = prompt("Pick a count 1-3.");
-		if (turn == 1 || turn == 2 || turn == 3){
+		if (count == 20 && turn == 2 || turn == 3){
+			alert("Invalid input, try again.");
+			goodTurn = false;
+		}
+		else if (turn == 1 || turn == 2 || turn == 3){
 			count += parseInt(turn);
 			goodTurn = true;
 		}
